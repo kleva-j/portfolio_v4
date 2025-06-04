41964a814584272fcc0cc941aacfe29ddccc5205
kleva-j <kasmickleva@gmail.com>
2025-06-04 15:14:43 +0100

    refactor: switch contentlayer for @next/mdx and upgrade next and react

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+import type { MotionProps as OriginalMotionProps } from "framer-motion";
+
+declare module "framer-motion" {
+  interface MotionProps extends OriginalMotionProps {
+    className?: string;
+  }
+}
