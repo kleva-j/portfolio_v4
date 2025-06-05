ba31cc85b6ce12fdf510ab58be0c36f126928f52
kleva-j <kasmickleva@gmail.com>
2025-06-05 14:22:01 +0100

    feat: update base page layout and add site config

---
+++ lib/site/config.ts
@@ -0,0 +1,26 @@
+export const siteConfig = {
+  name: "Michael Obasi",
+  description: "Developer, writer, and creator.",
+  url: "https://michaelobasi.dev",
+  author: "Michael Obasi",
+  github: "https://github.com/kleva-j",
+  github_username: "kleva-j",
+  version: "1.0.0",
+  navigation: {
+    "/": { name: "Home" },
+    "/post": { name: "Post" },
+    "/snippets": { name: "Snippets" },
+  },
+  openGraph: {
+    title: "Michael Obasi",
+    description: "Developer, writer, and creator.",
+    url: "https://michaelobasi.dev",
+    siteName: "Michael Obasi",
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    title: "kasmickleva",
+    card: "summary_large_image",
+  },
+};
