f59c6e81a9665c6332a355801573eac4370f7b6d
kleva-j <kasmickleva@gmail.com>
2025-06-05 14:19:37 +0100

    feat: setup shadcn components with config

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function formatDate(date: string) {
   const currentDate = new Date();
   const targetDate = new Date(date);
