400639acc4e43d24d8ea4a57f988f98fbe01cc6e
kleva-j <kasmickleva@gmail.com>
2025-06-05 07:59:36 +0100

    fix: failing build

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,15 @@
-import { allBlogs } from 'contentlayer/generated';
+import { allPost } from "@/lib/constant";
 
 export default async function sitemap() {
-  const blogs = allBlogs.map((post) => ({
+  const blogs = allPost.map((post) => ({
     url: `https://leerob.io/blog/${post.slug}`,
     lastModified: post.publishedAt,
   }));
 
-  const routes = ['', '/blog', '/guestbook', '/uses'].map(
-    (route) => ({
-      url: `https://leerob.io${route}`,
-      lastModified: new Date().toISOString().split('T')[0],
-    })
-  );
+  const routes = ["", "/blog", "/guestbook", "/uses"].map((route) => ({
+    url: `https://leerob.io${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
   return [...routes, ...blogs];
 }
