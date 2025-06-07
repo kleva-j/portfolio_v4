9e8c0762c3c715b0137d71728efe124acf4e0f33
kleva-j <kasmickleva@gmail.com>
2025-06-07 11:42:31 +0100

    feat: update sitemap and robots.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,27 @@
-import { allPost } from "@/lib/constant";
+import { DeploymentUrl } from "@/lib/utils";
+import { getPostSlugs } from "@/lib/post";
+
+import path from "node:path";
 
 export default async function sitemap() {
-  const blogs = allPost.map((post) => ({
-    url: `https://leerob.io/blog/${post.slug}`,
-    lastModified: post.publishedAt,
-  }));
+  const postsDirectory = path.join(process.cwd(), "content", "post");
 
-  const routes = ["", "/blog", "/snippets"].map((route) => ({
-    url: `https://leerob.io${route}`,
+  const postsSlugs = await getPostSlugs(postsDirectory);
+
+  const posts = await Promise.all(
+    postsSlugs.map(async (slug: string) => {
+      const { metadata } = await import(`@/content/post/${slug}.mdx`);
+      return {
+        url: `${DeploymentUrl}/post/${slug}`,
+        lastModified: new Date(metadata.updatedAt).toISOString().split("T")[0],
+      };
+    })
+  );
+
+  const routes = ["", "/post", "/snippets"].map((route) => ({
+    url: `${DeploymentUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes, ...posts];
 }
