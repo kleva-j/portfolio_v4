c3c277a8f0cc4c12e14e1cb71f15a6016a4d2f30
kleva-j <kasmickleva@gmail.com>
2025-06-14 20:27:34 +0100

    feat: add callout, table, proscard and conscard components

---
+++ components/snippet/callout.tsx
@@ -0,0 +1,14 @@
+import type { PropsWithChildren } from "react";
+
+interface CalloutProps extends PropsWithChildren {
+  emoji: string;
+}
+
+export function Callout({ emoji, children }: CalloutProps) {
+  return (
+    <div className="px-4 py-3 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm flex items-center text-neutral-900 dark:text-neutral-100 mb-8">
+      <div className="flex items-center w-4 mr-4">{emoji}</div>
+      <div className="w-full callout">{children}</div>
+    </div>
+  );
+}
--- components/snippet/index.ts
+++ components/snippet/index.ts
@@ -1,1 +1,4 @@
 export { SnippetWrapper } from "./wrapper";
+export { ProsCard } from "./pros-card";
+export { ConsCard } from "./cons-card";
+export { Callout } from "./callout";
