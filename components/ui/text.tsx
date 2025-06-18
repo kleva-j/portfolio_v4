ce2cf04e1bc9e167de869f5e090233ba270f8745
kleva-j <kasmickleva@gmail.com>
2025-06-18 14:52:06 +0100

    feat: update home page design

--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -10,7 +10,7 @@ const typographyVariants = cva("font-semibold tracking-tight font-heading", {
       h2: "text-3xl first:mt-0",
       h3: "text-2xl",
       h4: "text-xl",
-      p: "scroll-m-0 font-normal leading-7 [&:not(:first-child)]:mt-6 font-sans",
+      p: "scroll-m-0 font-normal leading-7 [&:not(:first-child)]:mt-4 font-sans",
       blockquote: "mt-6 border-l-2 pl-6 italic font-sans",
     },
   },
