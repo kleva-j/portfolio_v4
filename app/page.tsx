dc988acbe0948b15e33cca1509b10715f747e51b
kleva-j <kasmickleva@gmail.com>
2025-06-19 05:40:40 +0100

    feat: update home page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,32 @@
 import { AnimatedUnderline } from "@/components/ui/animated-underline";
 import { Text } from "@/components/ui/text";
 
+import ProfilePic from "app/michael.jpg";
+import Image from "next/image";
+
 export default async function Page() {
   return (
-    <section className="my-20">
-      <AnimatedUnderline
-        as="h1"
-        className="font-bold text-2xl tracking-tighter text-pretty mb-8 font-mono w-max"
+    <section className="my-20 px-4 flex flex-col gap-y-4">
+      <Text as="h1" variant="h1" className="font-semibold text-3xl lg:text-4xl">
+        Michael Obasi
+      </Text>
+      <Text
+        as="h3"
+        variant="h3"
+        className="font-medium text-xl lg:text-2xl tracking-tighter text-pretty font-mono w-max mb-4"
       >
-        Hi, I'm Michael
-      </AnimatedUnderline>
-      <div className="text-justify text-pretty text-muted-foreground">
-        <Text as="p">
+        <span className="">👋</span> Hi, I'm{" "}
+        <AnimatedUnderline className="inline-flex">Michael</AnimatedUnderline>
+      </Text>
+      <div className="text-lg text-pretty text-gray-700 dark:text-gray-400">
+        <Image
+          className="rounded-full ml-2 border border-dashed p-1 overflow-hidden size-[180px] object-cover object-top-left float-right"
+          alt="Michael Obasi"
+          src={ProfilePic}
+          height={180}
+          width={180}
+        />
+        <Text as="p" className="mt-0!">
           I'm a software engineer with a passion for building innovative and
           user-friendly applications.
         </Text>
@@ -20,14 +35,14 @@ export default async function Page() {
           solve real-world problems. I'm passionate about building applications
           that are both aesthetically pleasing and user-friendly.
         </Text>
+        <Text as="p" className="">
+          I'm excited about the prospect of working with a company that shares
+          my values and is committed to using technology to make a positive
+          impact on society. I'm looking for a company that will challenge me,
+          provide opportunities for growth and allow me to contribute to the
+          development of innovative solutions.
+        </Text>
       </div>
-      <Text as="p" className="text-muted-foreground">
-        I'm excited about the prospect of working with a company that shares my
-        values and is committed to using technology to make a positive impact on
-        society. I'm looking for a company that will challenge me, provide
-        opportunities for growth and allow me to contribute to the development
-        of innovative solutions.
-      </Text>
     </section>
   );
 }
